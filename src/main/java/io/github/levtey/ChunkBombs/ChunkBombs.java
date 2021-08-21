5e080c2cff6110a547ec324854985d306d380821
levtey <levtey@gmail.com>
2021-08-20 23:58:41 -0700

    fix a dupe :grimacing:

--- src/main/java/io/github/levtey/ChunkBombs/ChunkBombs.java
+++ src/main/java/io/github/levtey/ChunkBombs/ChunkBombs.java
@@ -149,6 +149,7 @@ public class ChunkBombs extends JavaPlugin implements Listener {
 		if (itemInHand.getType().equals(Material.valueOf(config.getString("item.material").toUpperCase()))
 				&& itemInHand.getItemMeta().getPersistentDataContainer().has(chunkBombKey, PersistentDataType.BYTE)) {
 			evt.setCancelled(true);
+			itemInHand.setAmount(itemInHand.getAmount() - 1);
 			if (evt.getPlayer().hasPermission("chunkbombs.use")) {
 				if (!Bukkit.getPluginManager().isPluginEnabled("Towny") || !config.getBoolean("inTownsOnly")
 						|| (TownyAPI.getInstance().getTownName(evt.getBlock().getLocation()) != null)) {
